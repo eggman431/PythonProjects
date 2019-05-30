04087ba67341ec7339abc0c6849863ef9bf98390
Egan Paul <43151807+eggman431@users.noreply.github.com>
2019-05-30 14:26:57 -0400

    Add files via upload

---
+++ PythonPrograms/colorspaces.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+
+def get_frame(cap, scaling_factor):
+	_, frame = cap.read()
+	frame = cv2.resize(frame, None, fx=scaling_factor, fy=scaling_factor, interpolation = cv2.INTER_AREA)
+	return frame
+
+if __name__=='__main__':
+	cap = cv2.VideoCapture(0)
+	scaling_factor = 0.5
+	while True:
+		frame = get_frame(cap, scaling_factor)
+		hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+		lower = np.array([0,30,60])
+		upper = np.array([20,150,255])
+		mask = cv2.inRange(hsv,lower,upper)
+		img_bitwise_and = cv2.bitwise_and(frame, frame, mask = mask)
+		img_median_blurred = cv2.medianBlur(img_bitwise_and, 5)
+		cv2.imshow("Input",frame)
+		cv2.imshow("Output",img_median_blurred)
+		key = cv2.waitKey(5)
+		if key == 27:
+			break
+	cv2.destroyAllWindows()
